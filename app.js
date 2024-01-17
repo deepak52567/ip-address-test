312ab3d4295242047a4bcb900cb5626bbfe80f3b
deepak <deepak@deepaks-MacBook-Pro.local>
2024-01-17 12:11:05 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const app = express();
 const IP = require('ip');
 
 app.get('/', (req, res) => {
-    const ipAddress = IP.address();
+    console.log(req.ip)
     const ipObject = {
       ipAddress: IP.address(),
-      isPrivate: IP.isPrivate(ipAddress),
-      isPublic: IP.isPublic(ipAddress),
-      isV4Format: IP.isV4Format(ipAddress),
-      isV6Format: IP.isV6Format(ipAddress)
+      reqIpAddress: req.ip
+      // isPrivate: IP.isPrivate(ipAddress),
+      // isPublic: IP.isPublic(ipAddress),
+      // isV4Format: IP.isV4Format(ipAddress),
+      // isV6Format: IP.isV6Format(ipAddress)
     }
     res.send(ipObject)
 })
