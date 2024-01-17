28a10a86cfdb1a3285ec6e7deaf7e43e42f946d6
deepak <deepak@deepaks-MacBook-Pro.local>
2024-01-17 16:15:46 +0530

    refactor

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const IP = require('ip');
 var requestIp = require('request-ip');
-var useragent = require('express-useragent');
-app.use(useragent.express());
+var expressUseragent = require('express-useragent');
+app.use(expressUseragent.express());
 
 app.get('/', (req, res) => {
+  console.log(req.useragent);
     const ipObject = {
       ipAddress: IP.address(),
       reqIPAddress: requestIp.getClientIp(req),
-      userAgent: req.useragent
+      userAgent: req.useragent || 'test'
     }
     res.send(ipObject)
 })
